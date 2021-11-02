45e8b94c47908c3cf1a61d05bf130340119ab322
FUJIAWESOME <dkorotchikov123@gmail.com>
2021-11-02 17:07:34 +0300

    calculator

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function calc (a, b, operator) {
+    const isNotValid = (isNaN(a) || isNaN(b) || !a || !b || !operator);
+    if (isNotValid){
+        return "Error";
+    }
+
+    if (operator === "multi"){
+        return a * b;
+    } else if (operator === "pow"){
+        return a ** b;
+    } else if (operator === "sum"){
+        return a + b;
+    } else if (operator === "div"){
+        return a / b;
+    } else if (operator === "rem"){
+        return a % b;
+    } else if (operator === "sub"){
+        return a - b;
+    }
+    
+    return "unknown operation";
+}
+
+console.log(calc(2, 3, "sum"))
+
