2c0774beca7f5cdb2a1c361b0cc8d77732cc8394
FUJIAWESOME <dkorotchikov123@gmail.com>
2021-11-02 20:25:23 +0300

    fixed4

--- calcSwitchCase.js
+++ calcSwitchCase.js
@@ -5,25 +5,25 @@ function calc (a, b, operator) {
     }
     switch(operator) {
         case "sum":
-            console.log(a + b); 
+            console.log(+a + +b); 
             break;
         case "pow": 
-            console.log(a ** b); 
+            console.log((+a) ** +b); 
             break;
         case "div": 
-            console.log(a / b); 
+            console.log(+a / +b); 
             break;
         case "multi": 
-            console.log(a * b); 
+            console.log(+a * +b); 
             break;
         case "rem": 
-            console.log(a % b); 
+            console.log(+a % +b); 
             break;
         case "sub": 
-            console.log(a - b); 
+            console.log(+a - +b); 
             break;
         default: 
             console.log("unknown operation");
     }
 }
-calc(0, 3, "sum")
+calc("2", "4", "pow")
--- calculator.js
+++ calculator.js
@@ -5,21 +5,21 @@ function calc (a, b, operator) {
     }
 
     if (operator === "multi"){
-        return a * b;
+        return +a * +b;
     } else if (operator === "pow"){
-        return a ** b;
+        return (+a) ** +b;
     } else if (operator === "sum"){
-        return a + b;
+        return +a + +b;
     } else if (operator === "div"){
-        return a / b;
+        return +a / +b;
     } else if (operator === "rem"){
-        return a % b;
+        return +a % +b;
     } else if (operator === "sub"){
-        return a - b;
+        return +a - +b;
     }
     
     return "unknown operation";
 }
 
-console.log(calc("", 3, "sum"))
+console.log(calc("2", "3", "pow"))
 
